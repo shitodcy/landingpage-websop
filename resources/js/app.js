a248c9a307503ede1d788cc4d105b905b37966e7
azunya <tn.putra404.com>
2026-05-19 17:47:33 +0700

    halaman helloworld

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,58 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const mascot = document.getElementById("anime-mascot");
+
+    function closeAllDropdowns() {
+        const memberToggles = document.querySelectorAll('.member-toggle');
+        memberToggles.forEach(toggle => {
+            const content = toggle.nextElementSibling;
+            const icon = toggle.querySelector('.toggle-icon');
+            
+            content.classList.add('hidden');
+            content.classList.remove('flex');
+            icon.style.transform = 'rotate(0deg)';
+        });
+    }
+
+    const btnTeam = document.getElementById('btn-team');
+    const modalTeam = document.getElementById('modal-team');
+    const btnCloseModal = document.getElementById('close-modal');
+
+    if (btnTeam && modalTeam && btnCloseModal) {
+
+        btnTeam.addEventListener('click', () => {
+            modalTeam.classList.remove('hidden');
+        });
+
+        btnCloseModal.addEventListener('click', () => {
+            modalTeam.classList.add('hidden');
+            closeAllDropdowns();
+        });
+
+        modalTeam.addEventListener('click', (e) => {
+            if (e.target === modalTeam) {
+                modalTeam.classList.add('hidden');
+                closeAllDropdowns();
+            }
+        });
+    }
+
+    const memberToggles = document.querySelectorAll('.member-toggle');
     
-    // Pastikan elemen mascot ada sebelum menjalankan script
-    if (!mascot) return;
+    memberToggles.forEach(toggle => {
+        toggle.addEventListener('click', function() {
+            const content = this.nextElementSibling;
+            const icon = this.querySelector('.toggle-icon');
+            
+            const isCurrentlyOpen = !content.classList.contains('hidden');
+ 
+            closeAllDropdowns();
 
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+            if (!isCurrentlyOpen) {
+                content.classList.remove('hidden');
+                content.classList.add('flex');
+                icon.style.transform = 'rotate(90deg)';
+            }
+        });
+    });
 
-    mascot.onmousedown = dragMouseDown;
-    mascot.ontouchstart = dragMouseDown;
-
-    function dragMouseDown(e) {
-        e.preventDefault();
-        
-        if (e.type === 'touchstart') {
-            pos3 = e.touches[0].clientX;
-            pos4 = e.touches[0].clientY;
-        } else {
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-        }
-        
-        mascot.classList.add('grabbing');
-        mascot.classList.remove('cursor-grab');
-        
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-        document.ontouchend = closeDragElement;
-        document.ontouchmove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e.preventDefault();
-        
-        let clientX = e.type === 'touchmove' ? e.touches[0].clientX : e.clientX;
-        let clientY = e.type === 'touchmove' ? e.touches[0].clientY : e.clientY;
-
-        pos1 = pos3 - clientX;
-        pos2 = pos4 - clientY;
-        pos3 = clientX;
-        pos4 = clientY;
-
-        mascot.style.top = (mascot.offsetTop - pos2) + "px";
-        mascot.style.left = (mascot.offsetLeft - pos1) + "px";
-        mascot.style.bottom = "auto";
-        mascot.style.right = "auto";
-    }
-
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-        document.ontouchend = null;
-        document.ontouchmove = null;
-        
-        mascot.classList.remove('grabbing');
-        mascot.classList.add('cursor-grab');
-    }
 });
